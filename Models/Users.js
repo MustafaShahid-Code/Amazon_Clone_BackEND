5c2bf7061ddc82cf313d5319c9325d7e1e52b09b
Mustafa Shahid <mustafashahid1991@gmail.com>
2020-12-07 13:45:11 +0300

    User Model Added!

---
+++ Models/Users.js
@@ -0,0 +1,40 @@
+const moment = require('moment');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const moment = require('moment')
+
+const UserSchema = new Schema({
+
+    UserName:{
+        type:String,
+        required: true
+    },
+    Email:{
+        type:String,
+        required:true
+    },
+    Password:{
+        type:String,
+        required:true
+    },
+    Profile_Pic:{
+        type:String,
+        default:"empty-avatar.jpg"
+    },
+    CreatedAt:{
+        type:String,
+        default: moment().format("DD/MM/YYYY") + ";"+ moment().format("hh:mm:ss")
+    },
+    UpdatedAt:{
+        type:String,
+        default: moment().format("DD/MM/YYYY") + ";"+ moment().format("hh:mm:ss")
+    }
+})
+
+//CreateUserModel
+
+mongoose.model("users",UserSchema);
+
+//ExportUserModel
+module.exports=mongoose.model("users")
+
